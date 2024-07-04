77090c9adc9fb3e17af5e5c036cca6005f679ece
johnson <k32720101@gmail.com>
2024-07-04 22:15:59 +0800

    update

--- W2_Uncertainly/HW/pagerank/test.py
+++
@@ -1,5 +0,0 @@
-from pagerank import transition_model
-corpus = {"1.html": {"2.html", "3.html"}, "2.html": {"3.html"}, "3.html": {"2.html"}}
-page = "1.html"
-x = transition_model(corpus, page, 0.85)
-print(x)
